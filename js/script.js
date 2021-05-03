2d41ed7df4f19221669650e7d3e34518eea741f5
Rayane Ben Haga <rayanebenhaga@gmail.com>
2021-05-03 23:01:34 +0200

    Add a writer

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@ function start() {
     hide("box")
     view("box1")
     hide("box3")
+    write()
 }
 
 function start1() {
@@ -52,3 +53,45 @@ function start1() {
     hide("box1")
     view("box2")
 }
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+function write() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
