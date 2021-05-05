8ebc24f2f1812a46d3996419bd5f6f2cbfb69080
Rayane Ben Haga <rayanebenhaga@gmail.com>
2021-05-05 22:31:53 +0200

    Finalisation of the project

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(function(){
     })
 })
 
-function hide(className) {
+function hideClass(className) {
     var appHandlers = document.getElementsByClassName(className);
 
     for (var i = 0; i < appHandlers.length; i ++) {
@@ -27,7 +27,7 @@ function hide(className) {
     }
 }
 
-function view(className) {
+function viewClass(className) {
     var appHandlers = document.getElementsByClassName(className);
 
     for (var i = 0; i < appHandlers.length; i ++) {
@@ -35,54 +35,198 @@ function view(className) {
     }
 }
 
+function viewID(idName) {
+    document.getElementById(idName).style.display = 'inline-block';
+}
+
+function hideID(idName) {
+    document.getElementById(idName).style.display = 'none';
+}
+
+
+var stop = false
+var isDeleting = false
+var fullTxt = ''
+var ele
+var that
+
+var introduction = "Depuis le vingt-et-unième siècle, nous avons de plus en plus tendance à vouloir faciliter et rendre plus attractif ce qui nous entoure en ajoutant de la liberté, et la littérature n’échappe pas à ce mouvement. Comment est-ce que la littérature numérique satisfait-elle cette volonté d’avoir plus de libertés que son équivalent classique ? D’abord nous verrons de quelle manière ce sentiment de liberté se fait ressentir à travers la littérature numérique. Ensuite nous observerons qu’il se voit limité sous plusieurs aspects. Ce n’est qu’une fois ces deux idées éclaircies que nous les confronterons."
+var partie1 = "Une soif de liberté chez les lecteurs est de plus en plus observée et les auteurs ne manquent pas d’y répondre via la création d'œuvres numériques variées dont leurs nombreux aspects favorisent la satisfaction de la requête d’origine. L’accessibilité de ces œuvres qui provient du format leur permet d’être consultées facilement et la gratuité aide d’autant plus. De plus, le lecteur peut également se voir doté de différents choix et ainsi avoir une sensation d'influence qui renforce son sentiment de liberté et de contrôle. Enfin, les interprétations étant plus larges et différentes offrent au lecteur un champ de réflexion agrandi en comparaison aux œuvres plus classiques qui généralement ne laissent qu’une légère ambiguïté quant à leur signification."
+var partie2 = "La littérature numérique, bien qu’accordant une liberté plus large qu’un format matériel, présente aussi ses limites. Parler de liberté n’est en fait qu’une illusion, le lecteur choisit de se déplacer comme il le souhaite dans le document mais dans la limite imposée par l’auteur. Les hyperfonctions permettent de se déplacer dans l'œuvre mais pas d’avoir cette notion de liberté car on est vraiment soumis aux conditions du programmeur. Ces liens hypertexte sont placés par l’auteur donc si il n’en propose pas, on est limité et l’expérience s’arrête là.  Il n’est donc pas question de liberté, mais plutôt d’une impression de liberté. "
+var partie3 = "La liberté à travers la littérature numérique existe même si elle est contrainte par de nombreux facteurs. Elle demeure bien plus présente dans ce courant littéraire que dans son homologue classique. Ce sentiment de liberté paraît par les nombreuses possibilités parmi plusieurs fonctionnalités, il est visible aussi avec l'interaction par laquelle nous pouvons avoir l'impression d’être maître du déroulement de l'œuvre. La liberté est donc associée à une sorte de prise de pouvoir même si celle-ci est contrôlée et encadrée. Elle est effectivement octroyée ainsi elle témoigne d’une évolution de la volonté des lecteurs et des mœurs de notre époque."
+var conclusion = "Nous venons d’opposer les deux parties et d’en faire le bilan, ainsi nous pouvons en tirer que la littérature numérique octroie effectivement au lecteur plus de liberté. En effet, les avantages sont plus nombreux que les inconvénients et ils répondent mieux aux attentes qu’un lecteur aurait en cherchant de la liberté. Nous pensons cependant que les fonctionnalités pourraient être plus creusées étant donné que l’on peut se sentir, au contraire, prisonnier de la trame artistique et scénaristique d’un auteur. Offrir plus de choix et y ajouter des débouchés différents pourraient réellement donner un sens plus profond à la littérature numérique et par conséquent renforcer le sentiment de liberté initialement demandé."
+
 function init() {
-    view("box")
-    hide("box1")
-    hide("box2")
+    viewClass("box")
+    viewClass("home-footer")
+
+    hideClass("box1")
+    hideClass("box2")
+    hideClass("box3")
+    hideClass("text-footer")
+    hideClass("end-footer")
 }
 
 function start() {
-    hide("box")
-    view("box1")
-    hide("box3")
+    hideClass("box")
+    hideClass("home-footer")
+
+    viewClass("box1")
+    viewClass("text-footer")
+
+    viewClass("intro")
+
+    viewID("skip")
+    viewID("next")
+    document.getElementById("next").onclick = part1;
+
+    fullTxt = introduction;
     write()
 }
 
-function start1() {
-    hide("box")
-    hide("box1")
-    view("box2")
+function intro() {
+    skip()
+
+    hideClass("part1")
+    hideClass("part2")
+    hideClass("part3")
+    hideClass("conclu")
+    viewClass("intro")
+
+    hideID("before")
+    hideID("dott")
+    hideID("end")
+    document.getElementById("next").onclick = part1;
+
+    next(introduction)
+}
+
+function part1() {
+    skip()
+
+    hideClass("intro")
+    hideClass("part2")
+    hideClass("part3")
+    hideClass("conclu")
+    viewClass("part1")
+
+    viewID("before")
+    viewID("dott")
+    document.getElementById("before").onclick = intro;
+    document.getElementById("next").onclick = part2;
+
+    next(partie1)
+}
+
+function part2() {
+    skip()
+
+    hideClass("intro")
+    hideClass("part1")
+    hideClass("part3")
+    hideClass("conclu")
+    viewClass("part2")
+
+    document.getElementById("before").onclick = part1;
+    document.getElementById("next").onclick = part3;
+
+    next(partie2)
+}
+
+function part3() {
+    skip()
+
+    hideClass("intro")
+    hideClass("part1")
+    hideClass("part2")
+    hideClass("conclu")
+    viewClass("part3")
+
+    document.getElementById("before").onclick = part2;
+    document.getElementById("next").onclick = conclu;
+
+    next(partie3)
+}
+
+function conclu() {
+    skip()
+
+    hideClass("intro")
+    hideClass("part1")
+    hideClass("part2")
+    hideClass("part3")
+    viewClass("conclu")
+
+    document.getElementById("before").onclick = part3;
+    document.getElementById("next").onclick = info;
+
+    next(conclusion)
+}
+
+function info() {
+    skip()
+
+    hideClass("box1")
+    viewClass("box2")
+    viewClass("end-footer")
+}
+
+function end() {
+    hideClass("end-footer")
+    hideClass("box2")
+    viewClass("box3")
+}
+
+function skip() {
+    ele.innerHTML = '<span class="wrap">'+ fullTxt +'</span>';
+    stop = true;
+}
+
+function next(text) {
+    ele.innerHTML = '<span class="wrap"></span>';
+    stop = false;
+    fullTxt = text;
+    write()
 }
 
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
-    this.el = el;
+    ele = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
     this.txt = '';
     this.tick();
-    this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+    that = this;
+    if(this.txt === fullTxt) {
+        stop = false;
+        that.stop();
+    }
 
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    if(stop) {
+        this.txt = fullTxt;
+        ele.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        stop = false;
+        that.stop();
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    ele.innerHTML = '<span class="wrap">' + this.txt +'</span>';
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    var delta = 100 - Math.random() * 100;
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
+    if(!stop) {
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    }
 };
 
 function write() {
     var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
         var period = elements[i].getAttribute('data-period');
         if (toRotate) {
